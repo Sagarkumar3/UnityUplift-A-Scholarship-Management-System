7daca450d79f5d54b23f6ee70fba3f6509d20ae4
SagarKumar3 <sk9834334@gmail.com>
2026-04-20 00:41:46 +0500

    FYP Project

---
+++ unity_uplift-main/lib/utils/app-constant.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = 'Investigate Market for new Investment';
+  static const appMainColor = Color(0xffF5591F);
+  static const appScendoryColor = Color(0xffF5591F);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+
+  static const stripePublishKey =
+      'pk_test_51PcDOV2MgrWgVX4zLRjPRbhEGdLgbkdFnfgkTzAk7qlpLAqlrY6aJ0avrnlPPjwNbjb1mqFOw9qpZE7YmqciB6JU00hfvkDyP6';
+  static const stripeSecretKey = 'YOUR_STRIPE_KEY_HERE';
+}
